6018d320c31e3dd96e0ce669bd4d872653128936
Hypernite <drunkenmonkey4987@gmail.com>
2023-05-08 20:41:07 -0400

    Included #73

---
+++ rektor/core/src/lib.rs
@@ -0,0 +1,19 @@
+//! # Core
+
+#![deny(clippy::all)]
+
+/// Adds two numbers together.
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
